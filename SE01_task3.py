a8c5f424540d7a420124ef46e674ebb74f7fb754
mafumafuteru <94123769+mafumafuteru@users.noreply.github.com>
2023-05-11 18:00:16 +0200

    Create SE01_task3.py

---
+++ SE01_task3.py
@@ -0,0 +1,45 @@
+import random
+
+COLORS = ["RED","ORANGE","YELLOW","GREEN","BLUE","PURPLE"]
+CODE = [random.choice(COLORS) for a in range(4)] 
+
+#lists of all possibe guesses 
+all_possible_guesses = []
+for a in COLORS:
+       for b in COLORS:
+              for c in COLORS:
+                     for d in COLORS:
+                            if len(set((a,b,c,d))):
+                                   all_possible_guesses.append((a,b,c,d))
+
+#the rest is the same like the task2, only inplemented something new above!
+
+for i in range(12):
+        guess = input("Please write four colors in the order you think(in upper case and seperate with spaces): ") .split()
+        if len(guess) != 4 or not all(color in COLORS for color in guess):
+            print("Please write FOUR VALID colors from the list: ", COLORS)
+            continue
+        correct = 0
+        misplaced = 0
+        #covert guess to a set to remove the duplicates
+        guess_set = set(guess)
+
+        for j in guess_set:
+               if j in CODE:
+                      #only count each color once
+                      if guess.count(j) <= CODE.count(j):
+                             misplaced += min(guess.count(j), CODE.count(j))
+        for j in range(4):
+                if guess[j]== CODE[j]:
+                        correct += 1
+                        #remove from misplaced count if it's also correct
+                        misplaced -= 1
+        if correct == 4:
+                print("Congrats! You break the code!")
+                break
+        else:
+                print(f"{correct} colors are correct and in the right positions!")
+                print(f"{misplaced} colors are right color but in the wrong place!")
+else:
+       print("Sorry, you run out of the chances to guess!") 
+       print(f"The correct code is:{''.join(CODE)}")
