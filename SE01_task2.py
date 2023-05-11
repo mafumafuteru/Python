b4a8494ecbfbea4fab44194a80a24a709c49f2f5
mafumafuteru <94123769+mafumafuteru@users.noreply.github.com>
2023-05-11 16:59:50 +0200

    Update SE01_task2.py

--- SE01_task2.py
+++ SE01_task2.py
@@ -1,25 +1,31 @@
 import random
 
 COLORS = ["RED","ORANGE","YELLOW","GREEN","BLUE","PURPLE"]
-CODE = random.sample(COLORS, k=4)
+CODE = [random.choice(COLORS) for a in range(4)] 
 
 for i in range(12):
-        guess = input("Please write four nonrepeat colors in the order you think(in upper case and seperate with spaces): ") .split()
+        guess = input("Please write four colors in the order you think(in upper case and seperate with spaces): ") .split()
         if len(guess) != 4 or not all(color in COLORS for color in guess):
-            print("Please write four valid colors from the list: ", COLORS)
+            print("Please write FOUR VALID colors from the list: ", COLORS)
             continue
         correct = 0
-        wrong_placed = 0
+        misplaced = 0
+        guess_set = set(guess)
+
+        for j in guess_set:
+               if j in CODE:
+                      if guess.count(j) <= CODE.count(j):
+                             misplaced += min(guess.count(j), CODE.count(j))
         for j in range(4):
                 if guess[j]== CODE[j]:
                         correct += 1
-                elif guess[j] in CODE:
-                        wrong_placed += 1
+                        misplaced -= 1
         if correct == 4:
                 print("Congrats! You break the code!")
                 break
         else:
                 print(f"{correct} colors are correct and in the right positions!")
-                print(f"{wrong_placed} colors are right color but in the wrong postion!")
+                print(f"{misplaced} colors are right color but in the wrong place!")
 else:
        print("Sorry, you run out of the chances to guess!") 
+       print(f"The correct code is:{''.join(CODE)}")
